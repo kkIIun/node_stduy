f4f8cf5e59c45988d3a265d556716d50bb3bc8f1
kkIIun <rlwjd4177@naver.com>
2021-07-30 03:45:35 +0900

    9: kakao로그인, local로그인 구현완료

---
+++ 9.js/passport/index.js
@@ -0,0 +1,21 @@
+const passport = require("passport");
+const local = require("./localStrategy");
+const kakao = require("./kakaoStrategy");
+const User = require("../models/user");
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    // 로그인 시 실행 됨
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    // 매 요청시 실행
+    User.findOne({ where: { id } })
+      .then((user) => done(null, user))
+      .catch((err) => done(err));
+  });
+
+  local();
+  kakao();
+};
