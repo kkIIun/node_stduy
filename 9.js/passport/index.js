9c634084aecd3221b2d04151f1436abdfa569183
kkIIun <rlwjd4177@naver.com>
2021-08-19 13:35:32 +0900

    9.js: deserializeUser 캐싱하기

--- 9.js/passport/index.js
+++ 9.js/passport/index.js
@@ -3,6 +3,9 @@ const local = require("./localStrategy");
 const kakao = require("./kakaoStrategy");
 const User = require("../models/user");
 
+let users = {};
+let expiredate = {};
+
 module.exports = () => {
   passport.serializeUser((user, done) => {
     // 로그인 시 실행 됨
@@ -11,23 +14,33 @@ module.exports = () => {
 
   passport.deserializeUser((id, done) => {
     // 매 요청시 실행
-    User.findOne({
-      where: { id },
-      include: [
-        {
-          model: User,
-          attributes: ["id"],
-          as: "Followers",
-        },
-        {
-          model: User,
-          attributes: ["id"],
-          as: "Followings",
-        },
-      ],
-    })
-      .then((user) => done(null, user))
-      .catch((err) => done(err));
+    const user = users[id];
+    const date = expiredate[id];
+    if(!user || !date || date < Date.now())
+      User.findOne({
+        where: { id },
+        include: [
+          {
+            model: User,
+            attributes: ["id", "nick"],
+            as: "Followers",
+          },
+          {
+            model: User,
+            attributes: ["id", "nick"],
+            as: "Followings",
+          },
+        ],
+      })
+        .then((user) => {
+          users[id] = user;
+          expiredate[id] = Date.now() + 5000;
+          done(null, user)
+        })
+        .catch((err) => done(err));
+    else {
+      done(null,user)
+    }
   });
 
   local();
