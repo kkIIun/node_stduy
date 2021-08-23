7be686738f494df675db6c75c885e5eb4eb89f3a
kkIIun <rlwjd4177@naver.com>
2021-08-24 00:08:33 +0900

    11.3, 11.4

--- 9.js/controllers/user.test.js
+++ 9.js/controllers/user.test.js
@@ -1,5 +1,5 @@
 jest.mock("../models/user");
-const { addFollowing } = require("./user");
+const { addFollowing, removeFollowing, updateUser } = require("./user");
 const User = require("../models/user");
 
 describe("addFollowing", () => {
@@ -36,6 +36,68 @@ describe("addFollowing", () => {
     const error = "테스트용 에러";
     User.findOne.mockReturnValue(Promise.reject(error));
     await addFollowing(req, res, next);
+    expect(next).toBeCalledWith(error);
+  });
+});
+
+describe("removeFollowing", () => {
+  const req = {
+    user: { id: 1 },
+    params: { id: 2 },
+  };
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(),
+  };
+  const next = jest.fn();
+
+  test("사용자를 찾아 팔로잉을 삭제하고 success를 응답해야 함.", async () => {
+    User.findOne.mockReturnValue(
+      Promise.resolve({
+        removeFollowing(id) {
+          return Promise.resolve(true);
+        },
+      })
+    );
+    await removeFollowing(req, res, next);
+    expect(res.send).toBeCalledWith("success");
+  });
+
+  test("사용자를 못 찾으면 res.status(404).send('no user')를 호출함.", async () => {
+    User.findOne.mockReturnValue(null);
+    await removeFollowing(req, res, next);
+    expect(res.status).toBeCalledWith(404);
+    expect(res.send).toBeCalledWith("no user");
+  });
+
+  test("DB에서 에러가 발생하면 next(error) 호출함", async () => {
+    const error = "테스트용 에러";
+    User.findOne.mockReturnValue(Promise.reject(error));
+    await removeFollowing(req, res, next);
+    expect(next).toBeCalledWith(error);
+  });
+});
+
+describe("updateUser", () => {
+  const req = {
+    body: { nick: "kijung" },
+    user: { id: 1 },
+  };
+  const res = {
+    json: jest.fn(),
+  };
+  const next = jest.fn();
+
+  test("사용자를 찾아 팔로잉을 삭제하고 success를 응답해야 함.", async () => {
+    User.update.mockReturnValue(Promise.resolve(true));
+    await updateUser(req, res, next);
+    expect(res.json).toBeCalledWith(true);
+  });
+
+  test("DB에서 에러가 발생하면 next(error) 호출함", async () => {
+    const error = "테스트용 에러";
+    User.update.mockReturnValue(Promise.reject(error));
+    await updateUser(req, res, next);
     expect(next).toBeCalledWith(error);
   });
 });
