28f72a2b1143e5ee9bfb54c3fb1a6a8e1ce93c99
kkIIun <rlwjd4177@naver.com>
2021-08-23 19:57:22 +0900

    11.1, 11.2

--- 9.js/routes/user.js
+++ 9.js/routes/user.js
@@ -1,48 +1,15 @@
 const express = require("express");
 const { isLoggedIn } = require("./middlewares");
-const { User } = require("../models");
+const {
+  addFollowing,
+  removeFollowing,
+  updateUser,
+} = require("../controllers/user");
 
 const router = express.Router();
 
-router.post("/:id/follow", isLoggedIn, async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { id: req.user.id } });
+router.post("/:id/follow", isLoggedIn, addFollowing);
+router.post("/:id/defollow", isLoggedIn, removeFollowing);
+router.patch("/update", isLoggedIn, updateUser);
 
-    if (user) {
-      await user.addFollowing(parseInt(req.params.id, 10));
-      res.send("success");
-    } else {
-      res.status(404).send("no user");
-    }
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-router.post("/:id/defollow", isLoggedIn, async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { id: req.user.id } });
-
-    if (user) {
-      await user.removeFollowing(parseInt(req.params.id, 10));
-      res.send("success");
-    } else {
-      res.status(404).send("no user");
-    }
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-router.patch("/update", isLoggedIn, async (req, res, next) => {
-  try {
-    const result = await User.update({ nick: req.body.nick }, { where: { id: req.user.id } });
-    res.json(result);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
 module.exports = router;
